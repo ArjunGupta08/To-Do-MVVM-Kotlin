983bdcad31f22f219c1c757df255ad641e7865ae
Arjun Gupta <arjungupta0817@gmail.com>
2024-03-23 22:39:41 +0530

    viewModelScope changed

--- app/src/main/java/com/arjungupta/mytodo/viewModel/MainViewModel.kt
+++ app/src/main/java/com/arjungupta/mytodo/viewModel/MainViewModel.kt
@@ -3,37 +3,48 @@ package com.arjungupta.mytodo.viewModel
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.arjungupta.mytodo.models.ToDoData
 import com.arjungupta.mytodo.repo.ToDoRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainViewModel(val context: Context) : ViewModel() {
+class MainViewModel(private val context: Context) : ViewModel() {
+
+    private val toDoRepository : ToDoRepository
+        get() = ToDoRepository()
 
     // Returns LiveData from Room
-    fun getData() : LiveData<List<ToDoData>> {
-        return ToDoRepository.getData(context)
+    fun getData() : LiveData<List<ToDoData>>? {
+        return toDoRepository.getData(context)
     }
 
     // Insert ToDoData to Room
     fun insertData(toDoData: ToDoData) {
-        CoroutineScope(Dispatchers.IO).launch {
-            ToDoRepository.insertData(toDoData, context)
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                toDoRepository.insertData(toDoData, context)
+            }
         }
     }
 
     // update ToDoData to Room
     fun updateData(toDoData: ToDoData) {
-        CoroutineScope(Dispatchers.IO).launch {
-            ToDoRepository.updateData(context, toDoData)
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                toDoRepository.updateData(context, toDoData)
+            }
         }
     }
 
     // Delete ToDoData to Room
     fun deleteData(toDoData: ToDoData) {
-        CoroutineScope(Dispatchers.IO).launch {
-            ToDoRepository.deleteData(toDoData, context)
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                toDoRepository.deleteData(toDoData, context)
+            }
         }
     }
 
